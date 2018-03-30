72c6fc45da038c9fd96978e8f3d411a9cc43371f
Steven Giraldo <stevengiraldo@outlook.com>
2018-03-30 18:05:50 -0500

    add event w/ ES6 mode

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,11 +3,7 @@ import PropTypes from 'prop-types';
 import './media.css';
 
 class Media extends Component {
-  constructor(props) {
-    super(props)
-    this.handleClick = this.handleClick.bind(this)
-  }
-  handleClick(event) {
+  handleClick = (event) => {
     console.log(this.props.title)
   }
   render() {

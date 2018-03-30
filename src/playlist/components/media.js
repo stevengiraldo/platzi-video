2be1ca57453bbf7d29020c366bcd5ae4e1c3c2e0
Steven Giraldo <stevengiraldo@outlook.com>
2018-03-30 17:15:43 -0500

    create first component

---
+++ src/playlist/components/media.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Media extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src='./images/covers/responsive.jpg' alt='' width={260} height={160} />
+        </div>
+        <h3>¿Por qué aprender Responsive Design?</h3>
+        <p>Steven Giraldo</p>
+      </div>
+    )
+  }
+}
+
+export default Media;

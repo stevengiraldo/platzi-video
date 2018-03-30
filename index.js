861256cff4bb0e089b59892999dcfcbd5d6f97c3
Steven Giraldo <stevengiraldo@outlook.com>
2018-03-30 17:17:27 -0500

    implemented a shorter import method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Media />, app);
+render(<Media />, app);

806027b477ae4fba3b0de522452dadc3c5b6146f
Steven Giraldo <stevengiraldo@outlook.com>
2018-03-31 11:31:45 -0500

    add composition of elements w/ Icon and Play

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Media from './media';
+import Play from '../../icons/components/play';
 import './playlist.css';
 
 function Playlist(props) {
   const playlist = props.playlist
   return (
     <div className='Playlist'>
+      <Play size={30} color='red' />
       {
         playlist.map((item) => {
           return <Media

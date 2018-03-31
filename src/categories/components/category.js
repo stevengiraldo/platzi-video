ccff3ae96a7e4d6063fc1ff1055e5e6a423ed4b9
Steven Giraldo <stevengiraldo@outlook.com>
2018-03-31 17:45:43 -0500

    add styles to categories and category

--- src/categories/components/category.js
+++ src/categories/components/category.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Playlist from '../../playlist/components/playlist';
+import './category.css';
 
 function Category(props) {
   return (
-    <div>
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
+    <div className='Category'>
+      <h2 className='Category-title'>{props.title}</h2>
+      <p className='Category-description'>{props.description}</p>
       <Playlist playlist={props.playlist} />
     </div>
   )

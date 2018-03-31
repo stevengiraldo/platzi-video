08827bb25b9b3c03bf8137d5608a316a8a2ebc37
Steven Giraldo <stevengiraldo@outlook.com>
2018-03-31 10:43:53 -0500

    add styles to Playlist

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Media from './media';
+import './playlist.css';
 
 class Playlist extends Component {
   render() {
     const playlist = this.props.data.categories[0].playlist
     return (
-      <div>
+      <div className='Playlist'>
         {
           playlist.map((item) => {
             return <Media

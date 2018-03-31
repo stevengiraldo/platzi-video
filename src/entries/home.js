8951a28fef5663695c0f0e436b6abd0a9dce5382
Steven Giraldo <stevengiraldo@outlook.com>
2018-03-31 17:08:01 -0500

    init structure smart/dumb component w/ home

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Category from '../category/components/category';
+import Home from '../pages/containers/home';
 import data from '../api.json';
 
 const app = document.getElementById('app');
 
-render(<Category data={data} />, app);
+render(<Home />, app);

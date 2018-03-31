95bc736fd32a00fdd3d03031387254df92104b8c
Steven Giraldo <stevengiraldo@outlook.com>
2018-03-31 11:40:36 -0500

    add Icon components

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Media from './media';
 import Play from '../../icons/components/play';
+import Volume from '../../icons/components/volume';
+import Pause from '../../icons/components/pause';
+import Fullscreen from '../../icons/components/fullscreen';
 import './playlist.css';
 
 function Playlist(props) {
   const playlist = props.playlist
   return (
     <div className='Playlist'>
+      <Volume size={40} color='yellow' />
+      <Pause size={35} color='blue' />
       <Play size={30} color='red' />
+      <Fullscreen size={25} color='green' />
       {
         playlist.map((item) => {
           return <Media

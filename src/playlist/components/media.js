5bd7aa37168073b8ee600ee4602bf1d164e8fbeb
Steven Giraldo <stevengiraldo@outlook.com>
2018-03-31 11:12:15 -0500

    convert Media and Playlist  to functional components

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './media.css';
 
-class Media extends Component {
-  render() {
-    return (
-      <div className='Media'>
-        <div className='Media-cover'>
-          <img className='Media-img'
-            src={this.props.cover}
-            alt=''
-            width={260}
-            height={160}
-          />
-        </div>
-        <h3 className='Media-title'>{this.props.title}</h3>
-        <p className='Media-author'>{this.props.author}</p>
+function Media(props) {
+  return (
+    <div className='Media'>
+      <div className='Media-cover'>
+        <img className='Media-img'
+          src={props.cover}
+          alt=''
+          width={260}
+          height={160}
+        />
       </div>
-    )
-  }
+      <h3 className='Media-title'>{props.title}</h3>
+      <p className='Media-author'>{props.author}</p>
+    </div>
+  )
 }
 
 Media.propTypes = {
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Media from './media';
 import './playlist.css';
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.playlist
-    return (
-      <div className='Playlist'>
-        {
-          playlist.map((item) => {
-            return <Media
-              {...item}
-              key={item.id}
-            />
-          })
-        }
-      </div>
-    )
-  }
+function Playlist(props) {
+  const playlist = props.playlist
+  return (
+    <div className='Playlist'>
+      {
+        playlist.map((item) => {
+          return <Media
+            {...item}
+            key={item.id}
+          />
+        })
+      }
+    </div>
+  )
 }
 
 export default Playlist;

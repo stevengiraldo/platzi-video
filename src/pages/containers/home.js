8f82e3a223ed35ba8c00c334e8a1c8318d0335e7
Steven Giraldo <stevengiraldo@outlook.com>
2018-04-02 22:24:33 -0500

    create portal w/ Modal

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,6 +4,7 @@ import Categories from '../../categories/components/categories';
 import Related from '../components/related';
 import { myPlaylist } from '../../my-playlist.json';
 import { friends } from '../../friends.json';
+import ModalContainer from '../../widgets/containers/modal';
 
 class Home extends Component {
   render() {
@@ -11,6 +12,9 @@ class Home extends Component {
       <HomeLayout>
         <Related myPlaylist={myPlaylist} friends={friends} />
         <Categories categories={this.props.data.categories} />
+        <ModalContainer>
+          <h1>Esto es un portal</h1>
+        </ModalContainer>
       </HomeLayout>
     )
   }

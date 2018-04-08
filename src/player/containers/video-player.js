24c76b0233963c2b7fbca59832152a51c2a51f7b
Steven Giraldo <stevengiraldo@outlook.com>
2018-04-08 10:09:19 -0500

    WIP: add play-pause to video component

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component {
+  state = {
+    pause: false,
+  }
+  togglePlay = (event) => {
+    this.setState({
+      pause: !this.state.pause
+    })
+  }
   render() {
     return (
       <VideoPlayerLayout>
         <Title title='Esto es un video' />
+        <PlayPause 
+          pause={this.state.pause}
+          handleClick={this.togglePlay}
+        />
         <Video />
       </VideoPlayerLayout>
     )
